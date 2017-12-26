b8dd54db7308001bae7d4833e2bd7868b8034207
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-12-26 15:04:10 +0300

    Initialize project

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/AlexKbit/reactive-spring.svg?branch=develop)](https://travis-ci.org/AlexKbit/reactive-spring)
+[![codecov.io](https://codecov.io/github/AlexKbit/reactive-spring/coverage.svg?branch=develop)](https://codecov.io/gh/AlexKbit/reactive-spring?branch=develop)
+
+# Project Overview #
+
+TODO
+
---
+++ src/test/java/com/alexkbit/reactivespring/intro/ApplicationLauncherTests.java
@@ -0,0 +1,16 @@
+package com.alexkbit.reactivespring.intro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationLauncherTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
